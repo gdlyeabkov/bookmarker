3c343092f218fd8fc42f3bf556aa219419706bc8
gdlyeabkov <gdlyeabkov@gmail.com>
2023-05-14 18:38:12 +0300

    prepare_layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,60 +1,27 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app>
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      color="cyan">
+      <v-app-bar-title>
+        <img
+         src="https://www.diigo.com/asset/images/logo_s2@2x.png"
+         width="75" />
+      </v-app-bar-title>
+      <v-btn class="text-capitalize mx-2" color="transparent" elevation="0">My Library</v-btn>
+      <v-btn class="text-capitalize mx-2" color="transparent" elevation="0">My Outliners</v-btn>
+      <v-btn class="text-capitalize mx-2" color="transparent" elevation="0">My Groups</v-btn>
+      <v-btn class="text-capitalize mx-2" color="transparent" elevation="0">Tools</v-btn>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <v-btn class="text-capitalize mx-4" color="orange">Upgrade</v-btn>
+      <v-avatar color="info" size="24">
+        <v-img src="https://resources.diigo.com/images/avatar/user/nickfish2020_96.jpg?rand=863" />
+      </v-avatar>
+      <v-btn class="text-capitalize" color="transparent" elevation="0">account</v-btn>
+      <v-btn icon>
+        <v-icon>mdi-chevron-down</v-icon>
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -62,28 +29,55 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
+      color="rgb(245, 245, 245)"
       :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      app>
+      <v-row justify="center" no-gutters>
+        <span class="black--text ">&copy; {{ new Date().getFullYear() }} Diigo Inc.</span>
+        <v-btn
+          color="transparent"
+          elevation="0"
+          variant="text"
+          class="black--text text-capitalize mx-2">
+          <span>About</span>
+        </v-btn>
+        <v-btn
+          color="transparent"
+          elevation="0"
+          variant="text"
+          class="black--text text-capitalize mx-2">
+          <span>Terms of Service</span>
+        </v-btn>
+        <v-btn
+          color="transparent"
+          elevation="0"
+          variant="text"
+          class="black--text text-capitalize mx-2">
+          <span>Privacy Policy</span>
+        </v-btn>
+        <v-btn
+          color="transparent"
+          elevation="0"
+          variant="text"
+          class="black--text text-capitalize mx-2">
+          <span>Blog</span>
+        </v-btn>
+        <v-btn
+          color="transparent"
+          elevation="0"
+          variant="text"
+          class="black--text text-capitalize mx-2">
+          <span>Twitter</span>
+        </v-btn>
+        <v-btn
+          color="transparent"
+          elevation="0"
+          variant="text"
+          class="black--text text-capitalize mx-2">
+          <span>Contact Us</span>
+        </v-btn>
+      </v-row>
     </v-footer>
   </v-app>
 </template>
@@ -93,24 +87,10 @@ export default {
   data () {
     return {
       clipped: false,
-      drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
       miniVariant: false,
       right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      rightDrawer: false
     }
   }
 }
