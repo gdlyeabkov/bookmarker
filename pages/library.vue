37e5df29795f55a6c76bc81422ddb67e9667fa3d
gdlyeabkov <gdlyeabkov@gmail.com>
2023-05-20 17:47:27 +0300

    add_icons_for_text_field_in_library_and_add_fab_in_library

--- pages/library.vue
+++ pages/library.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="width: 100%;">
     <v-toolbar
+      height="75"
       style="width: 100%;"
       fixed
       color="cyan">
@@ -8,17 +9,37 @@
         <v-btn class="text-capitalize mx-2" color="transparent" elevation="0">Моя библиотека</v-btn>
       </v-toolbar-title>
       <v-col cols="2">
-        <v-text-field dense>
-          <template v-slot:label>
-            <span>
-              <v-icon icon="mdi-file-find"></v-icon>
-            </span>
-          </template>
+        <v-text-field dense prepend-icon="mdi-magnify" append-icon="mdi-menu-down" placeholder="Поиск">
         </v-text-field>
       </v-col>
       <v-spacer />
       <v-btn class="text-capitalize mx-4" color="transparent" elevation="0">Collapse All</v-btn>
       <v-btn class="text-capitalize mx-4" color="transparent" elevation="0">Bulk Edit</v-btn>
+      <v-menu transition="fab-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            class="text-capitalize mx-4 mt-8"
+            color="blue"
+            rounded
+            width="50"
+            min-width="50"
+            max-width="50"
+            height="50"
+            min-height="50"
+            max-height="50"
+            v-bind="props">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+        <v-list-item
+          v-for="n in 5"
+          :key="n"
+        >
+          <v-list-item-title>{{`Item ${n}`}}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+      </v-menu>
     </v-toolbar>
   </div>
 </template>
