6e1872cdba3e6ea92d83ebd343d6e7ab056654c6
gdlyeabkov <gdlyeabkov@gmail.com>
2023-05-24 16:02:49 +0300

    separate_header_to_component

---
+++ components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app-bar
+    elevation="0"
+    fixed
+    app
+    color="cyan">
+    <v-app-bar-title>
+      <img
+        src="https://www.diigo.com/asset/images/logo_s2@2x.png"
+        width="75" />
+    </v-app-bar-title>
+    <v-btn class="text-capitalize mx-2" color="transparent" elevation="0" @click="switchScreen('library')">Моя библиотека</v-btn>
+    <v-btn class="text-capitalize mx-2" color="transparent" elevation="0" @click="switchScreen('outliners')">Мои структуры</v-btn>
+    <v-btn class="text-capitalize mx-2" color="transparent" elevation="0" @click="switchScreen('groups')">Мои группы</v-btn>
+    <v-btn class="text-capitalize mx-2" color="transparent" elevation="0" @click="switchScreen('tools')">Инструменты</v-btn>
+    <v-spacer />
+    <v-btn class="text-capitalize mx-4" color="orange" :rounded="true">Улучшить</v-btn>
+    <v-avatar color="info" size="24">
+      <v-img src="https://resources.diigo.com/images/avatar/user/nickfish2020_96.jpg?rand=863" />
+    </v-avatar>
+    <v-btn class="text-capitalize" color="transparent" elevation="0">аккаунт</v-btn>
+    <v-menu>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          icon
+          v-bind="attrs"
+          v-on="on">
+          <v-icon>mdi-chevron-down</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item class="clickable" @click="switchScreen('profile')">
+          <v-list-item-title>Публичная библиотека</v-list-item-title>
+        </v-list-item>
+        <v-list-item class="clickable" @click="switchScreen('settings')">
+          <v-list-item-title>Настройки</v-list-item-title>
+        </v-list-item>
+        <v-list-item class="clickable" @click="switchScreen('support')">
+          <v-list-item-title>Поддержка</v-list-item-title>
+        </v-list-item>
+        <v-list-item class="clickable" @click="switchScreen('discover')">
+          <v-list-item-title>Обнаружить</v-list-item-title>
+        </v-list-item>
+        <v-list-item class="clickable" @click="switchScreen('help')">
+          <v-list-item-title>Помощь</v-list-item-title>
+        </v-list-item>
+        <v-list-item class="clickable" @click="logout()">
+          <v-list-item-title>Выйти</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    switchScreen (screen) {
+      this.$router.push(screen)
+    },
+    logout () {
+      this.switchScreen('start')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
