9acc6c0e54a16d8a6ba87a9121c9a2d22f4c2dff
gdlyeabkov <gdlyeabkov@gmail.com>
2023-05-29 17:40:38 +0300

    add_footer_to_component

---
+++ components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-footer
+    color="rgb(245, 245, 245)"
+    app>
+    <v-row justify="center" no-gutters>
+      <span class="grey--text my-2">&copy; {{ new Date().getFullYear() }} Diigo Inc.</span>
+      <v-btn
+        color="transparent"
+        elevation="0"
+        variant="text"
+        class="grey--text text-capitalize mx-2">
+        <span>О нас</span>
+      </v-btn>
+      <v-btn
+        color="transparent"
+        elevation="0"
+        variant="text"
+        class="grey--text text-capitalize mx-2">
+        <span>Условия использования</span>
+      </v-btn>
+      <v-btn
+        color="transparent"
+        elevation="0"
+        variant="text"
+        class="grey--text text-capitalize mx-2">
+        <span>Политика конфиденциальности</span>
+      </v-btn>
+      <v-btn
+        color="transparent"
+        elevation="0"
+        variant="text"
+        class="grey--text text-capitalize mx-2">
+        <span>Блог</span>
+      </v-btn>
+      <v-btn
+        color="transparent"
+        elevation="0"
+        variant="text"
+        class="grey--text text-capitalize mx-2">
+        <span>Twitter</span>
+      </v-btn>
+      <v-btn
+        color="transparent"
+        elevation="0"
+        variant="text"
+        class="grey--text text-capitalize mx-2">
+        <span>Связаться с нами</span>
+      </v-btn>
+    </v-row>
+  </v-footer>
+</template>
+<script>
+export default {
+  setup () {
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,63 +6,17 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      color="rgb(245, 245, 245)"
-      app>
-      <v-row justify="center" no-gutters>
-        <span class="grey--text my-2">&copy; {{ new Date().getFullYear() }} Diigo Inc.</span>
-        <v-btn
-          color="transparent"
-          elevation="0"
-          variant="text"
-          class="grey--text text-capitalize mx-2">
-          <span>О нас</span>
-        </v-btn>
-        <v-btn
-          color="transparent"
-          elevation="0"
-          variant="text"
-          class="grey--text text-capitalize mx-2">
-          <span>Условия использования</span>
-        </v-btn>
-        <v-btn
-          color="transparent"
-          elevation="0"
-          variant="text"
-          class="grey--text text-capitalize mx-2">
-          <span>Политика конфиденциальности</span>
-        </v-btn>
-        <v-btn
-          color="transparent"
-          elevation="0"
-          variant="text"
-          class="grey--text text-capitalize mx-2">
-          <span>Блог</span>
-        </v-btn>
-        <v-btn
-          color="transparent"
-          elevation="0"
-          variant="text"
-          class="grey--text text-capitalize mx-2">
-          <span>Twitter</span>
-        </v-btn>
-        <v-btn
-          color="transparent"
-          elevation="0"
-          variant="text"
-          class="grey--text text-capitalize mx-2">
-          <span>Связаться с нами</span>
-        </v-btn>
-      </v-row>
-    </v-footer>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
